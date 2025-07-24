c9a464f7fddbf3ec3bb9959885b617f7b6e3b006
KamalSharma13 <kamal.webdev13@gmail.com>
2025-07-25 03:42:16 +0530

    updated

--- server/api/v1/controllers/cronjob/cronIndex.js
+++ server/api/v1/controllers/cronjob/cronIndex.js
@@ -1,22 +1,21 @@
 /* ---For cronService only--- */
-require('./tickers');
-require('./tradeFee');
-require('./coins');
+ require('./tickers');
+ require('./tradeFee');
+ require('./coins');
 // require('./totalProfitAndTotalTradeAndVolumeCron')
 // require('./profitStatistic')
-require('./withdrawFee');
+ require('./withdrawFee');
 require("./poolRewardDistribution")
 require("./poolRewardUpdation")
 
 /* ---For master only--- */
  require('./pathArbitrage');
-// require('./autoTrade');
-// require('./sniperAuto')
-
-/* ---For TradeCronService only--- */
-// require('./triangularTrade');
-// require('./directTrade');
-// require('./intraSingleExchangeTrade')
+ require('./autoTrade');
+ require('./sniperAuto')
+//* ---For TradeCronService only--- */
+ require('./triangularTrade');
+ require('./directTrade');
+ require('./intraSingleExchangeTrade')
 
 
 
